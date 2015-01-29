b07f9e6e0b349b2c48689eac0c137b9e2de26184
rodzewich <rodzewich.oleg@gmail.com>
2015-01-29 19:36:40 +0300

    -

--- src/ini.ts
+++ src/ini.ts
@@ -4,11 +4,38 @@ import utils = require("utils");
 
 module ini {
 
-    export function encode(text: string): any {}
+    export function encode(text: string): any {
+        var lines: string[] = String(text || "").split(/\r?\n/),
+            length: number = lines.length,
+            section: string,
+            result: any,
+            index: number,
+            line: string;
+        for (index = 0; index < length; index++) {
+            line = lines[index];
+            if (true) {
+                // set section
+            } else if (true) {
+                // set value
+            } else if (true) {
+                // show error
+            }
+        }
+        return result;
+    }
 
     export function decode(object: any, sections?: string[]): string {
+        var index: number,
+            length: number,
+            section: string;
         if (utils.typeOf(sections) === "array") {
-
+            length = sections.length;
+            for (index = 0; index < length; index++) {
+                section = String(sections[index] || "");
+                // todo: check section name
+            }
+        } else if (utils.typeOf(sections) !== "undefined") {
+            throw new Error("bla bla bla");
         }
         return null;
     }
